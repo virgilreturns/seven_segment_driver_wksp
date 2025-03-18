ab25ab896b3c81ce5cafa339291858d79ae279ba
Anderson Nguyen <nguyenanderson4@gmail.com>
2025-03-18 15:39:49 -0400

    change debounce algorithm to integrator

--- seven_segment_driver/Drivers/GPIO_driver.c
+++ seven_segment_driver/Drivers/GPIO_driver.c
@@ -1,8 +1,19 @@
 #include "GPIO_driver.h"
 
-static inline bool GPIO_DigitalRead(GPIO_TypeDef *port, uint16_t pin) { // polled with a timer ISR
+
+// ISR should call every CHECK_MSEC
+static inline bool GPIO_DigitalRead(GPIO_TypeDef* port, uint16_t pin) {
+	static bool isDebounced = false;
+	static
+
+
+
+	
+
+
+	//debounce control, change state type or return (condition) to fine tune
 	static uint32_t State = 0;
 	State = (State << 1) | HAL_GPIO_ReadPin(port, pin) | 0xFFFE0000;
-	return (State == 0xFFFF0000) {
+	return (State == 0xFFFF0000);
 }
 
--- seven_segment_driver/Drivers/GPIO_driver.h
+++ seven_segment_driver/Drivers/GPIO_driver.h
@@ -1,3 +1,12 @@
 #include "main.h"
+#define CHECK_PERIOD 5
+#define DEBOUNCE_TIME 0.200 // seconds
+#define INTEGRATOR_MAXIMUM ((CHECK_PERIOD)*(DEBOUNCE_TIME))
 
-bool GPIO_DigitalRead(GPIO_TypeDef* port, uint16_t pin)
+
+bool GPIO_DigitalRead(GPIO_TypeDef* port, uint16_t pin, uint8_t* integrator) {
+	static uint8_t raw; = HAL_GPIO_ReadPin()
+		if (raw == 0) integrator > 0 ? integrator--;
+		else if (integrator < INTEGRATOR_MAXIMUM) integrator++;
+
+}
